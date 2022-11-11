ec16101d12a23a6f3eab2066f1c930757c4d867c
(jmkim) <skawjdals96@gmail.com>
2022-11-11 17:36:49 +0900

    Create Entity

--- jpashop/src/test/java/jpabook/jpashop/MemberRepositoryTest.java
+++ jpashop/src/test/java/jpabook/jpashop/MemberRepositoryTest.java
@@ -8,7 +8,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
-
+/*
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class MemberRepositoryTest {
@@ -34,3 +34,4 @@ public class MemberRepositoryTest {
     }
 
 }
+*/
